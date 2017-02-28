5d5c9b88e69eba4a3b075285c10dbfa1a163dac8
Matt Flowers <mattmflowers28@gmail.com>
2017-02-28 01:50:57 -0500

    Added a comma

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
