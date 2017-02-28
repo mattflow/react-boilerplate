8bb3c62f2ca2e5c557bd1dd9776da126fc4a546f
Matt Flowers <mattmflowers28@gmail.com>
2017-02-28 12:52:30 -0500

    Modified setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-cd ../../
-rm -rf .git && git init
-git add . && git commit -m "Initial commit"
-
+cd ../../ && 
+rm -rf .git && 
+git init && 
+git add . && 
+git commit -m "Initial commit" &&
 npm install
