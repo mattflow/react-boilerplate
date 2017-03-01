edf9d319ef9ff2fe1c984a66b5a765f8f9d36e58
Matt Flowers <mattmflowers28@gmail.com>
2017-03-01 02:53:55 -0500

    Fixed directory issue

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,7 @@ npm install &&
 cd scripts &&
 rm setup.sh &&
 echo -e "#!/usr/bin/env bash\necho setup already complete" >> setup.sh &&
+cd ../ &&
 git add . && 
 git commit -m "Initial commit" &&
 echo -e "setup complete\n\nrun:\tnpm run start\nto open the dev server"
